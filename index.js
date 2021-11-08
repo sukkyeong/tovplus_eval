8c6eb56c0defb4aa1c926e75098b20b7bc9d2a50
Sukkyeong-Park <skpark@voistore.com>
2021-11-08 16:20:26 +0900

    config commit

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = 5000
+const config = require('./config/key');
+
+const { User } = require('./models/User');
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://voicerec:poi098)(*@tovpluseval.u3c4r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
-    useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
-}).then(()=> console.log('MongoDB Connected..') )
+mongoose.connect(config.mongoURI).then(()=> console.log('MongoDB Connected..') )
   .catch(err => console.log(err));
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! SK')
+})
+
+app.post('/register',(req,res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if(err) return res.json({success:false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://192.168.130.60:${port}`)
 })
